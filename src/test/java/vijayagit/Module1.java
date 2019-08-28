8810d2fc7353e48462970ba2618827ae4f567b07
vijaya04 <gvlakshmi1990@gmail.com>
2019-08-28 19:24:12 +0530

    added vijaya git project

---
+++ src/test/java/vijayagit/Module1.java
@@ -0,0 +1,19 @@
+package vijayagit;
+
+import org.testng.annotations.Test;
+
+public class Module1 {
+	
+	
+	
+	@Test
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println(c);
+	}
+
+}
